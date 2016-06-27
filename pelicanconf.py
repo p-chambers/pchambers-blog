57bbd3a0eb1734a6fdaf25d5d3b3ff1183727de7
p-chambers <P.R.Chambers@soton.ac.uk>
2016-06-27 17:22:46 +0100

    First content commit

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Paul Chambers'
+SITENAME = u'P Chambers'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# # Blogroll
+LINKS = (('occ-airconics docs', 'http://occ-airconics.readthedocs.io/en/latest/'),
+         ('GitHub', 'https://github.com/p-chambers'))
+
+# # Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/pr_chambers'), 
+          )
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
